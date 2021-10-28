8b61999f4e3ba361ad1b9ca12cc3a0d67458408a
Pavel1711 <45904265+Pavel1711@users.noreply.github.com>
2021-10-28 12:42:07 +0300

    Добавил изображения в ячейки

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
 
-    let itemsPerRows: CGFloat = 2
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+
+    let itemsPerRows: CGFloat = 3
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.showsVerticalScrollIndicator = false
@@ -21,16 +23,18 @@ class PhotosCollectionViewController: UICollectionViewController {
         return 1
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-                
-        cell.backgroundColor = .black
-        
+
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+
+        cell.dogImageView.image = image
+
         return cell
     }
 }
